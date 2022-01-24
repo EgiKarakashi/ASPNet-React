bd95df5fbebd11a6b27ab661bb2152dc65a40b51
EgiKarakashi <61650064+EgiKarakashi@users.noreply.github.com>
2022-01-24 23:16:48 +0100

    New Commit

--- ASPNetServer/Program.cs
+++ ASPNetServer/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
             builder
                 .AllowAnyMethod()
                 .AllowAnyHeader()
-                .WithOrigins("http://localhost:3000", "https://appname.azurestaticapps.net");
+                .WithOrigins("http://localhost:3000", "https://ashy-plant-0a630ac03.1.azurestaticapps.net");
         });
 });
 
